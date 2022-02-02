06f7ddf166352b02cc6a872cd89e8331e4ab7b7c
Malempati Varun <67381413+varun437@users.noreply.github.com>
2022-02-02 21:54:56 +0530

    Create variables.tf

---
+++ Public & Private - Subnet/variables.tf
@@ -0,0 +1,13 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {}
+//vpc
+variable "vpc_cidr" {}
+variable "vpc_name" {}
+variable "environment" {}
+//public-subnet
+variable "public_subnet1_cidr" {}
+variable "public_subnet1_name" {}
+//private-subnet
+variable "private_subnet1_cidr" {}
+variable "private_subnet1_name" {}
