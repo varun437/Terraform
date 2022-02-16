93c68a40de04c569299031b99369c8263fd83b10
Malempati Varun <67381413+varun437@users.noreply.github.com>
2022-02-16 12:27:58 +0530

    Create variables.tf

---
+++ EC2/variables.tf
@@ -0,0 +1,23 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {}
+//vpc
+variable "vpc_cidr" {}
+variable "vpc_name" {}
+variable "environment" {}
+//public-subnet
+variable "public_subnet1_cidr" {}
+variable "public_subnet1_name" {}
+//private-subnet
+variable "private_subnet1_cidr" {}
+variable "private_subnet1_name" {}
+//public-route-table
+variable "Main_Routing_Table" {}
+//private-route-table
+variable "Private_Main_Routing_Table" {}
+//internet-gateway
+variable "IGW_name" {}
+//ec2-instance
+variable "ami_id" {}
+variable "instance_type" {}
+variable "key" {}
